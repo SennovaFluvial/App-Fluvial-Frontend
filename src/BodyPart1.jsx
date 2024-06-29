a73d863edc0bce030d1f27ec2204c075dbb99b84
EnriqueAlfonsov1 <alfonso07amaya@gmail.com>
2024-06-29 11:52:14 -0500

    Nueva sección agregada al home page

--- src/BodyPart1.jsx
+++ src/BodyPart1.jsx
@@ -12,9 +12,40 @@ export const BodyPart1 = () => {
                 <p className="parrafo">
                     Aquí te ofrecemos una innovadora aplicación para conectar de manera más eficiente a todos los actores y partes interesadas tanto en el sector privado como estatal.
                 </p>
+
+            </div>
+
+            <div className="contenedorEstilos">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-4 text-center">
+                            <i class="fa-solid fa-ship iconoP1"></i>
+                        </div>
+                        <div className="col-md-4">
+                            <h1 className='text-center titulo'>QUE TE OFRECEMOS</h1>
+                            <p className='mt-5 parrafo'>
+                                Nuestra solución está diseñada para transformar la manera en la que participantes del transporte fluvial interactúan.
+                            </p>
+                            <p className="parrafo">
+                                Proporcionando una plataforma centralizada que facilita la comunicación.
+                            </p>
+                        </div>
+                        <div className="col-md-4 text-center">
+                            <i class="fa-solid fa-users iconoP1"></i>
+                        </div>
+                    </div>
+                </div>
             </div>
 
 
+
+
+
+
+
+
+
+            w
         </>
     )
 }
