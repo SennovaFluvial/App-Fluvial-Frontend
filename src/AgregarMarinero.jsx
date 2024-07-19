3e070205144d7b81119a74130ac280f284bc13e6
FernandoAlfonso07 <tiringistingis52@gmail.com>
2024-07-19 13:32:10 -0500

    Ajustes formulario de agregar marinero

--- src/AgregarMarinero.jsx
+++ src/AgregarMarinero.jsx
@@ -32,7 +32,7 @@ export const AgregarMarinero = () => {
                     <div className="row my-4">  {/* Identificacion */}
                         <div className="col-md-5">
                             <div className="form-group">
-                                <label htmlFor="type_document">Identificación</label>
+                                <label htmlFor="type_document">Identificación <i class="fa-solid fa-hand-pointer ms-4"></i></label>
                                 <select id="tipo" name='type_document' className="form-control" defaultValue="">
                                     <option value="">Tipo de documento</option>
                                     <option value="0">C.C</option>
@@ -42,7 +42,7 @@ export const AgregarMarinero = () => {
                         </div>
                         <div className="col-md-7">
                             <div className="form-group">
-                                <label htmlFor="num_document">Numero de Documento</label>
+                                <label htmlFor="num_document">Numero de Documento <i class="fa-solid fa-id-card ms-4"></i></label>
                                 <input type="text" name='num_document' className="form-control" id="num_document" />
                             </div>
                         </div>
@@ -58,12 +58,12 @@ export const AgregarMarinero = () => {
                         <div className="col-md-5">
                             <div className="form-group">
                                 <label htmlFor="code_phone">Codigo de área</label>
-                                <input type="text" name='code_phone' className="form-control" id="code_phone" />
+                                <input type="text" name='code_phone' className="form-control" placeholder='ej. +57' id="code_phone" />
                             </div>
                         </div>
                         <div className="col-md-7">
                             <div className="form-group">
-                                <label htmlFor="number_phone">Numero de telefono</label>
+                                <label htmlFor="number_phone">Numero de telefono <i class="fa-solid fa-phone ms-4"></i></label>
                                 <input type="text" name='number_phone' className="form-control" id="number_phone" />
                             </div>
                         </div>
@@ -73,13 +73,23 @@ export const AgregarMarinero = () => {
                     <div className="row my-4">  {/* Contacto | correo */}
                         <div className="col-md-12">
                             <div className="form-group">
-                                <label htmlFor="mail">Correo</label>
+                                <label htmlFor="mail">Correo <i class="fa-solid fa-envelope"></i></label>
                                 <input type="email" name='mail' className="form-control" id="correo" />
                             </div>
                         </div>
                     </div>
+
+                    <div className="row my-4">  {/* adjunto de imagen */}
+                        <div className="col-md-12">
+                            <div className="form-group">
+                                <label htmlFor="img-of-sailor">Licencia (opcional) <i class="fa-solid fa-folder-plus ms-4"></i> </label>
+                                <input type="file" name='img-of-sailor' className="form-control file-img" id="img-of-sailor" />
+                            </div>
+                        </div>
+                    </div>
+
                     <div className="text-center">
-                        <button type="submit" className="btn btn-success">Guardar</button>
+                        <button type="submit" className="btn btn-success">Guardar <i class="fa-solid fa-floppy-disk ms-2"></i></button>
                     </div>
                 </form >
             </div >
