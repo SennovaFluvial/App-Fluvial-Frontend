ca9d98b25f3b80b7b20b29a23dce540d6bcf4ca6
FernandoAlfonso07 <tiringistingis52@gmail.com>
2024-08-30 12:14:58 -0500

    Cambio de logo

--- src/components/menu/agregar/AddCrew.jsx
+++ src/components/menu/agregar/AddCrew.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react"
 import { Outlet } from "react-router"
+import { useNavigate } from "react-router";
 export const AddCrew = () => {
+
+    const [first, setfirst] = useState("");
+    const nav = useNavigate();
+    const handleChange = (e) => {
+
+        const url = e.target.value;
+
+        nav("/adminSection/`${e}`")
+
+    }
+
     return (
         <>
             <div className="container">
                 <select className="form-select my-5" aria-label="Default select example">
                     <option defaultValue="">Seleccione...</option>
-                    <option value="captain">Capitán</option>
-                    <option value="sailor">Marinero</option>
-                    <option value="boatDriver">Motorista</option>
+                    <option value="add-captain">Capitán</option>
+                    <option value="add-sailor">Marinero</option>
+                    <option value="add-boat-driver">Motorista</option>
                 </select>
             </div>
 
